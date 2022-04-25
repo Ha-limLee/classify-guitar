6d4738bee91a65ecae3dfed38a6b98883309abff
Ha-limLee <leehalim09@gmail.com>
2022-04-25 14:39:05 +0900

    deleted unused functions

--- index.ts
+++ index.ts
@@ -1,16 +1,14 @@
 import express from 'express';
 import path from 'path';
-import multer from 'multer';
-import {spawn} from 'child_process';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, 'client/build')));  // react build directory
+// react build directory
+app.use(express.static(path.join(__dirname, 'client/build')));
+// model topology(.json) and sharded weights(.bin)
+// see: https://www.tensorflow.org/js/tutorials/conversion/import_keras
 app.use(express.static(path.join(__dirname, 'tfjs_model')));
-app.use('/uploads' ,express.static(path.join(__dirname, 'uploads')));   // images that client uploaded
-
-const upload = multer({dest: 'uploads/', limits: {fileSize: 5 * 1024 * 1024}});
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/client/build/index.html'));
@@ -18,30 +16,6 @@ app.get('/', (req, res) => {
 
 app.get('/get/model', (req, res) => {
     res.sendFile(path.join(__dirname, 'tfjs_model/model.json'));
-});
-
-app.post('/post/images', upload.array('img'), (req, res) => {
-    console.log(req.files);
-    /**
-     * todo:
-     * run.py
-     * 1. classify images in /uploads using tensorflow-cpu
-     * 2. print result
-     * index.ts
-     * 3. respond with the result
-     * react
-     * 4. show images by class. use <image src=''
-     *      see: https://stackoverflow.com/questions/15772394/how-to-upload-display-and-save-images-using-node-js-and-express
-     */
-    const child = spawn('python', ['run.py', `${PORT}`]);
-    child.stdout.on('data', (data) => {
-        console.log(data.toString());
-        res.json({ok: true});
-    });
-    child.stderr.on('data', (data) => {
-        console.log(data.toString());
-        res.json({ok: false});
-    });
 });
 
 app.listen(PORT, () => {
